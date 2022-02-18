dfeeed01e5df8f04a5f05e5fe3e42ba37b762891
will <will@theapiguys.com>
2022-02-18 12:56:39 -0500

    fix import

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='x509generator',
-    version='0.3',
+    version='0.4',
     packages=['x509generator'],
-    url='https://github.com/codinlikewilly/py-certgenerator',
+    url='https://github.com/codinlikewilly/x509generator',
     license='MIT ',
     author='will',
     author_email='will@theapiguys.com',
     description='Generates SSL x509 certificates',
-    download_url="https://github.com/codinlikewilly/x509generator/archive/refs/tags/v_03.tar.gz",
+    download_url="https://github.com/codinlikewilly/x509generator/archive/refs/tags/v_04.tar.gz",
     keywords=['x509', 'SSL', 'OpenSSL'],
     install_requires=[
         'pyopenssl',
